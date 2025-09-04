de43222a4dd3d5a015b68677a80d246846d0b53f
radufurtuna <151870447+radufurtuna@users.noreply.github.com>
2025-09-04 11:05:11 +0300

    Adaugare SettingsIcon pentru optiuni

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import SettingsIcon from './SettingsIcon'; 
 
 function SearchBar({ searchTerm, setSearchTerm }) {
+
   return (
-    <input
-      type="text"
-      placeholder="Caută după model..."
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
-      className="search-bar"
-    />
+    <div className="search-bar-container">
+      <input
+        type="text"
+        placeholder="Caută după model..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="search-bar"
+      />
+      <SettingsIcon /> {/* Iconița */}
+    </div>
   );
 }
 
